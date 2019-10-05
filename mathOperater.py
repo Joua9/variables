7fa8a5dd61e89b1a794fd5148ef4481d80b932e8
Joua9 <55327155+Joua9@users.noreply.github.com>
2019-10-04 21:06:44 -0500

    Add files via upload

---
+++ mathOperater.py
@@ -0,0 +1,48 @@
+'''
+Created on Oct 4, 2019
+
+@author: ITAUser
+'''
+a=2
+b=7
+c=a+b 
+print(c) 
+k=" joua "
+n=" lor "
+v=k+n 
+print(v) 
+
+c = b - a 
+bucket1= 2
+bucket2= [1,2,4]
+print(bucket1-bucket2[1])
+
+b = 30 
+c =3
+print(b*c) 
+rooms=5
+studentsPerClass=30
+print("the total number of students is " + str(rooms*studentsPerClass))
+
+x=8
+y=9 
+print(x/y)
+s=[3,2,9]
+f=[3,3,2]
+print(s[2]/f[1])
+
+z=x//y 
+print(z)
+d=30
+e=3 
+f=d//e 
+print("f equals" + str(f))
+
+f=d%e
+print("f reminder" + str(f)) 
+q=8
+p=2
+print(q%p)
+
+
+
