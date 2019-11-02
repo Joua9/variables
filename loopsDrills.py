322f2f5ea00ddb9a92ac06bedce582142f8b7f9e
Joua9 <55327155+Joua9@users.noreply.github.com>
2019-11-02 12:21:11 -0500

    Add files via upload

---
+++ loopsDrills.py
@@ -0,0 +1,78 @@
+'''
+For this assignment you should read the task, then below the task do what it asks you to do
+based on what the task tells you do first.
+EXAMPLE TASK:
+'''
+#EX) Declare a variable set to 3. Make a while loop that prints the variable 
+#    you just created and decrements the variable by one each time through
+#    the loop. Meanwhile, make the loop run until the variable you created 
+#    equals 0.
+i = 3;
+while i > 0:
+    print(i)
+    i -= 1
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+'''While Loops'''
+#1) Declare a variable set to 4. Make a while loop that prints the variable 
+#    you just created and decrements the variable by one each time through
+#    the loop. Meanwhile, make the loop run until the variable you created 
+#    equals 1.
+a = 4 
+while a >= 1:
+    print (a)
+    a -=1
+
+
+#2) Declare a variable set to 14. Make a while loop that prints the variable 
+#    you just created and increments the variable by one each time through
+#    the loop. Meanwhile, make the loop run until the variable you created 
+#    equals 20.
+z = 14 
+while z <= 20:
+    print (z)
+    z +=1
+    
+#3) Declare a variable set to 55. Make a while loop that prints the variable 
+#   you just created. Then make an if statement that makes the loop break when
+#   the variable is equal to 50. 
+j = 55
+while 55>= 50:
+    print (j) 
+    if j == 50:
+        break
+    j -=1
+
+'''For Loops'''
+#4) Create a list named sports. Put three sports into the list. Create
+#   a for loop that prints each sport in the list
+sports = ["golf", "tennis", "basketball"]
+for sport in sports:
+    print(sport)
+    
+#5) Create a for loop that loops through each letter in a string of one of your
+#   favorite songs. Each iteration should print should a letter of the word. 
+songs= "Try"
+for song in songs:
+    print(song)
+
+#6) Create a list named movies. Put five of your favorite movies into the list.
+#   However, make sure one of the movies is Avatar. 
+#   Create a for loop that iterates over the list. In the loop print the movie
+#   being looped over, but create an if statement that breaks out of the 
+#   loop if it is Avatar.
+movies = ["Finding Nemo", "Coco", "Avatar", "It", "The Big Fish"]
+for movie in movies:
+    print(movie)
+    if movie == "Avatar":
+        break
+
+
+
